a7f507d51a007545b72ac3cdb99c00c6a1dfe13e
Mark Miyashita <markm@yelp.com>
2013-07-25 12:32:57 -0700

    added libraries

--- iPadPingPong/Classes/Models/PPUser.h
+++ iPadPingPong/Classes/Models/PPUser.h
@@ -7,7 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <RestKit/RestKit.h>
 
 @interface PPUser : NSObject
+
+@property (nonatomic, copy) NSNumber *userId;
+@property (nonatomic, copy) NSString *badgeId;
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSNumber *skillLevel;
+
++ (RKObjectMapping *)mapping;
++ (RKResponseDescriptor *)usersResponseDescriptor;
++ (RKResponseDescriptor *)userResponseDescriptor;
 
 @end
--- iPadPingPong/Classes/Models/PPUser.m
+++ iPadPingPong/Classes/Models/PPUser.m
@@ -10,4 +10,25 @@
 
 @implementation PPUser
 
++ (RKObjectMapping *)mapping {
+  RKObjectMapping *userMapping = [RKObjectMapping mappingForClass:[self class]];
+  [userMapping addAttributeMappingsFromDictionary:@{
+   @"user_id" : @"userId",
+   @"badge_id" : @"badgeId",
+   @"name" : @"name",
+   @"skill_level" : @"skillLevel",
+   }];
+  return userMapping;
+}
+
++ (RKResponseDescriptor *)userResponseDescriptor {
+  RKResponseDescriptor *descriptor = [RKResponseDescriptor responseDescriptorWithMapping:self.mapping pathPattern:nil keyPath:@"user" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+  return descriptor;
+}
+
++ (RKResponseDescriptor *)usersResponseDescriptor {
+  RKResponseDescriptor *descriptor = [RKResponseDescriptor responseDescriptorWithMapping:self.mapping pathPattern:nil keyPath:@"users" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+  return descriptor;
+}
+
 @end
