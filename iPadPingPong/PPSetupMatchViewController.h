e48d58d622351e635f14ac1c207bcb7359076667
Alton Zheng-Xie <altonzheng@berkeley.edu>
2013-07-26 13:29:30 -0700

    stuff

--- iPadPingPong/PPSetupMatchViewController.h
+++ iPadPingPong/PPSetupMatchViewController.h
@@ -7,7 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+@class PPUser;
+
+//@protocol SetupMatchDelegate <NSObject>
+//
+//-(void)secondPlayerDidBadgeIn:(PPUser *)secondPlayer;
+//
+//@end
 
 @interface PPSetupMatchViewController : UIViewController
+
+@property (nonatomic, copy) PPUser *playerOneUser;
+@property (nonatomic, copy) PPUser *playerTwoUser;
+
+-(void)firstPlayerDidBadgeIn:(PPUser *)firstPlayer;
+-(void)secondPlayerDidBadgeIn:(PPUser *)secondPlayer;
 
 @end
--- iPadPingPong/PPSetupMatchViewController.m
+++ iPadPingPong/PPSetupMatchViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "PPSetupMatchViewController.h"
-
+#import "PPUser.h"
 @interface PPSetupMatchViewController ()
 
 @end
@@ -27,30 +27,63 @@
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
-	self.view.backgroundColor = [UIColor blueColor];
+	self.view.backgroundColor = [UIColor whiteColor];
 	
-	UILabel *playerOne = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 0, 0)];
-	UILabel *playerTwo = [[UILabel alloc] initWithFrame:CGRectMake(350, 420, 0, 0)];
+	UILabel *playerTwo = [[UILabel alloc] initWithFrame:CGRectMake(180, 420, 0, 0)];
 	UIImageView *vsImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"vs.png"]];
-	vsImage.frame = CGRectMake(200, 220, 100, 100);
-	playerOne.text = @"Player 1";
-	playerOne.backgroundColor = [UIColor clearColor];
-	playerOne.textColor = [UIColor whiteColor];
-	playerTwo.text = @"Player 2";
-	playerTwo.backgroundColor = [UIColor clearColor];
-	playerTwo.textColor = [UIColor whiteColor];
-	playerOne.font = [UIFont fontWithName:@"Helvetica Neue" size:40.0];
-	playerTwo.font = [UIFont fontWithName:@"Helvetica Neue" size:40.0];
 
-	[playerOne sizeToFit];
-	[playerTwo sizeToFit];
-	[self.view addSubview:vsImage];
-	[self.view addSubview: playerOne];
-	[self.view addSubview: playerTwo];
+	vsImage.frame = CGRectMake(200, 220, 100, 100);
 	
 	
 	// Do any additional setup after loading the view.
 }
+
+-(void)firstPlayerDidBadgeIn:(PPUser *)firstPlayer {
+	UILabel *playerOne = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 0, 0)];
+	UIImageView *playerOneImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"player1.jpg"]];
+	playerOneImage.frame = CGRectMake(-600, 50, 170, 300);
+	playerOne.text = firstPlayer.name;
+	playerOne.font = [UIFont fontWithName:@"Helvetica Neue" size:40.0];
+	playerOne.backgroundColor = [UIColor clearColor];
+	playerOne.textColor = [UIColor blueColor];
+	[playerOne sizeToFit];
+	[self.view addSubview: playerOne];
+	[self.view addSubview:playerOneImage];
+
+	[UIView animateWithDuration:0.6 animations:^{
+		playerOneImage.frame = CGRectMake(10, 50, 170, 300);
+	}];
+}
+
+-(void)secondPlayerDidBadgeIn:(PPUser *)secondPlayer {
+	UILabel *playerTwo = [[UILabel alloc] initWithFrame:CGRectMake(300, 420, 0, 0)];
+	UIImageView *playerTwoImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"player2.jpg"]];
+	playerTwoImage.frame = CGRectMake(1200, 450, 170, 300);
+	playerTwo.text = secondPlayer.name;
+	playerTwo.backgroundColor = [UIColor clearColor];
+	playerTwo.textColor = [UIColor redColor];
+	playerTwo.font = [UIFont fontWithName:@"Helvetica Neue" size:40.0];
+	
+	[playerTwo sizeToFit];
+	[self.view addSubview:playerTwo];
+	[self.view addSubview:playerTwoImage];
+	[UIView animateWithDuration:0.6 animations:^{
+		playerTwoImage.frame = CGRectMake(300, 450, 170, 300);
+	}];
+	
+	UIButton *player1Button = [[UIButton alloc] initWithFrame:CGRectMake(20, 450, 300, 100)];
+	UIButton *player2Button = [[UIButton alloc] initWithFrame:CGRectMake(20, 450, 300, 100)];
+
+//	[player2Button addTarget:self action:@selector(<#selector#>) forControlEvents:<#(UIControlEvents)#>]
+	player1Button.titleLabel.text = @"Player 1 Won";
+	player2Button.titleLabel.text = @"Player 2 Won";
+	
+	[self.view addSubview:player1Button];
+	[self.view addSubview:player2Button];
+	
+
+}
+
 
 - (void)didReceiveMemoryWarning
 {
